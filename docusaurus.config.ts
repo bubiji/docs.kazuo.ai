857e6869ac6c31f3deab3d74484c3e997ec7eb12
Lupino <lmjubuntu@gmail.com>
2024-05-10 23:19:48 +0800

    no throw broken links, just warn

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,6 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.kazuo.ai',
   url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
@@ -22,7 +21,7 @@ const config: Config = {
   organizationName, // Usually your GitHub org/user name.
   projectName, // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
